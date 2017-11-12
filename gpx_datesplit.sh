7d69af0c2dd0cf135b12616607c535f5b6c7cf2b
ranfyy <ranfyy@users.noreply.github.com>
2017-11-12 09:03:13 +0100

    tab -> spaces

--- gpx_datesplit.sh
+++ gpx_datesplit.sh
@@ -3,7 +3,7 @@ DBASE="2017-09-"
 SRCH="C-Team"
 for DAY in $(seq -w 03 29) ; do
     DSTR=$DBASE$DAY
-	FNOUT=$DSTR.gpx
+    FNOUT=$DSTR.gpx
     head --lines 20      $FNIN | sed 's|name>'$SRCH'|name>'$DSTR'|' > $FNOUT
     grep $DSTR -B 2 -A 1 $FNIN >> $FNOUT
     tail --lines  3      $FNIN >> $FNOUT
